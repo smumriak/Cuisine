7ab581e80772fc88d3f4f835f7bc4a160761bbc4
Serhii Mumriak <sergiy.mumryak@gmail.com>
2022-12-31 00:31:40 -0800

    State is hashable

--- Sources/Cuisine/Infrastructure/Context/State.swift
+++ Sources/Cuisine/Infrastructure/Context/State.swift
@@ -8,11 +8,19 @@
 import TinyFoundation
 
 @propertyWrapper
-public final class State<Value> {
+public final class State<Value>: Hashable {
     @Synchronized
     public var wrappedValue: Value
     
     public init(_ wrappedValue: Value) {
         self.wrappedValue = wrappedValue
+    }
+
+    public static func == (lhs: State<Value>, rhs: State<Value>) -> Bool {
+        ObjectIdentifier(lhs) == ObjectIdentifier(rhs)
+    }
+
+    public func hash(into hasher: inout Hasher) {
+        ObjectIdentifier(self).hash(into: &hasher)
     }
 }
